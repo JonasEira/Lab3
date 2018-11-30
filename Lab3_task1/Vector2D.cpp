432a2b5a7ddc649c6dd4a206237a0533166db5c4
Jonas Eira <joneir-8@student.ltu.se>
2018-11-30 11:05:25 +0100

    Add project files.

---
+++ Lab3_task1/Vector2D.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Vector2D.h"
+
+
+Vector2D::Vector2D()
+{
+}
+
+
+Vector2D::~Vector2D()
+{
+}
---
+++ Lab3_task1/Vector2D.h
@@ -0,0 +1,14 @@
+#pragma once
+class Vector2D
+{
+private:
+	float vector[2];
+
+public:
+	Vector2D();
+	Vector2D(float x, float y);
+	~Vector2D();
+	getX();
+	setX();
+};
+
