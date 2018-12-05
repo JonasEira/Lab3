a07dd357e7e239eee7085c6391e91cf7570e4970
Jonas Eira <jonas.eira@gmail.com>
2018-12-05 18:37:34 +0100

    Multi fixed, added unity

--- Lab3_task1/Matrix2D.cpp
+++ Lab3_task1/Matrix2D.cpp
@@ -1,25 +1,26 @@
 #include "pch.h"
 #include "Matrix2D.h"
-
+#include <iostream>
 
 Matrix2D::Matrix2D()
 {
 }
 
+
 Matrix2D::Matrix2D(float x1, float y1, float x2, float y2)
 {
-	matrix[0][0] = x1;
-	matrix[0][1] = y1;
-	matrix[1][0] = x2;
-	matrix[1][1] = y2;
+	data[0][0] = x1;
+	data[0][1] = y1;
+	data[1][0] = x2;
+	data[1][1] = y2;
 }
 
-Matrix2D::Matrix2D(Matrix2D &m)
+Matrix2D::Matrix2D(const Matrix2D& m)
 {
-	matrix[0][0] = m.matrix[0][0];
-	matrix[0][1] = m.matrix[0][1];
-	matrix[1][0] = m.matrix[1][0];
-	matrix[1][1] = m.matrix[1][1];
+	data[0][0] = m.data[0][0];
+	data[0][1] = m.data[0][1];
+	data[1][0] = m.data[1][0];
+	data[1][1] = m.data[1][1];
 }
 
 Matrix2D::~Matrix2D() { }
@@ -28,25 +29,36 @@ Matrix2D::~Matrix2D() { }
 Matrix2D & Matrix2D::operator*(Matrix2D & m)
 {
 	Matrix2D result;
-	result.matrix[0][0] = matrix[0][0] * m.matrix[0][0] + matrix[1][0] * m.matrix[0][1];
-	result.matrix[0][1] = matrix[0][0] * m.matrix[1][0] + matrix[1][0] * m.matrix[1][1];
-	result.matrix[1][0] = matrix[1][0] * m.matrix[0][0] + matrix[1][1] * m.matrix[0][1];
-	result.matrix[1][1] = matrix[1][0] * m.matrix[1][0] + matrix[1][1] * m.matrix[1][1];
+	result.data[0][0] = data[0][0] * m.data[0][0] + data[1][0] * m.data[0][1];
+	result.data[0][1] = data[0][0] * m.data[1][0] + data[1][0] * m.data[1][1];
+	result.data[1][0] = data[0][1] * m.data[0][0] + data[1][1] * m.data[0][1];
+	result.data[1][1] = data[0][1] * m.data[1][0] + data[1][1] * m.data[1][1];
 	return result;
 }
 
 void Matrix2D::setData(float x1, float y1, float x2, float y2)
 {
-	this->matrix[0][0] = x1;
-	this->matrix[0][1] = y1;
-	this->matrix[1][0] = x2;
-	this->matrix[1][1] = y2;
+	this->data[0][0] = x1;
+	this->data[0][1] = y1;
+	this->data[1][0] = x2;
+	this->data[1][1] = y2;
 }
 
 float* Matrix2D::getData()
 {
-	float* data = *this->matrix;
+	float* data = *this->data;
 	return data;
 }
 
+Matrix2D unity()
+{
+	Matrix2D m;
+	m.setData(1.0,0.0, 0.0, 1.0);
+	return m;
+}
 
+void Matrix2D::printMatrix()
+{
+	std::cout << "X:\tY:\n" << data[0][0] << "\t" << data[0][1] << "\n" << data[1][0] << "\t" << data[1][1] << "\n" << std::endl;
+
+}
--- Lab3_task1/Matrix2D.h
+++ Lab3_task1/Matrix2D.h
@@ -3,14 +3,15 @@
 class Matrix2D
 {
 private:
-	float matrix[2][2];
+	float data[2][2];
 
 public:
 
 	Matrix2D(float x1, float x2, float y1, float y2);
-	Matrix2D(Matrix2D & m);
+	Matrix2D(const Matrix2D & m);
 	Matrix2D();
 	
+	void printMatrix();
 	
 	void setData(float x1, float y1, float x2, float y2);
 	
@@ -20,4 +21,5 @@ public:
 
 	~Matrix2D();
 };
+Matrix2D unity();
 
